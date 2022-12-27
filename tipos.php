eb52f564624886c79de0c2f362f9f8ed015a299a
TassoFreno <118383460+TassoFreno@users.noreply.github.com>
2022-12-27 13:49:15 -0300

    ccc

--- tipos.php
+++ tipos.php
@@ -1,15 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tipos Php</title>
+    <link rel="stylesheet" href="">
 </head>
 <body>
     <?php
+    $n = 4;
+    $no = "Gustavo"
+    echo $no;
 
-    ?>
-    
+?>
+
+
+
 </body>
+
+
 </html>
+
